2f32dafc0de3d935863659a7c2f448d7ea186058
Karla-Silva <mkarla@outlook.com>
2022-10-26 18:59:09 -0300

    commit dvd criado

--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -2,9 +2,9 @@ public class DoacaoMidia{
     /*
     Criar classe DoacaoMidia
 
-Essa classe deve conter método pegarMidiaAleatoria() que devolve uma midia já criada na biblioteca aleatória
+Essa classe deve conter método pegarMidiaAleatoria() que devolve uma midia já criada na biblioteca
+aleatória
 
 Esse classe deve conter método assisitirFilme(Dvd dvd) e chamar os dois métodos de legenda
      */
-
 }
--- src/Dvd.java
+++ src/Dvd.java
@@ -2,16 +2,62 @@ public class Dvd extends Midia{
     /*
 
 Atributos:
-
 idioma : String
 possuiLegenda : boolean
 idiomaLegenda : String
-Métodos
 
+Métodos:
 sobrescrever darPlay(): exibir a mensagem "filme iniciado"
 construtor(todos os argumentos)
 get() e set()
 legenda(boolean ligar, String idioma) : liga a legenda e atualiza o idioma da legenda
 legenda(boolean ligarDesligar) : apenas atualiza a variável possuiLegenda
      */
+    private String idioma;
+    private boolean possuiLegenda;
+    private String idiomaLegenda;
+
+    public Dvd(String codigo, Double preco, String nome, String idioma, boolean possuiLegenda, String idiomaLegenda) {
+        super(codigo, preco, nome);
+        this.idioma = idioma;
+        this.possuiLegenda = possuiLegenda;
+        this.idiomaLegenda = idiomaLegenda;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public boolean isPossuiLegenda() {
+        return possuiLegenda;
+    }
+
+    public void setPossuiLegenda(boolean possuiLegenda) {
+        this.possuiLegenda = possuiLegenda;
+    }
+
+    public String getIdiomaLegenda() {
+        return idiomaLegenda;
+    }
+
+    public void setIdiomaLegenda(String idiomaLegenda) {
+        this.idiomaLegenda = idiomaLegenda;
+    }
+
+    @Override
+    public void darPlay(){
+        System.out.println("Filme iniciado");
+    }
+
+    public void legenda(boolean ligar, String idioma){
+        //liga a legenda e atualiza o idioma da legenda
+    }
+
+    public void legenda(boolean ligarDesligar){
+        //apenas atualiza a variável possuiLegenda
+    }
 }
