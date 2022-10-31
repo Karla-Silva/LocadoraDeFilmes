1dd30b05279bd6092858ed8d070fc0bae2652db5
MayaCarv <mayaracarvalho@hotmail.com>
2022-10-31 10:49:40 -0300

    DoacaoMidia criada

--- src/DoacaoMidia.java
+++ src/DoacaoMidia.java
@@ -1,11 +1,29 @@
 package GIT.FinalProject.src;
-public class DoacaoMidia{
-    /*
-    Criar classe DoacaoMidia
 
-Essa classe deve conter método pegarMidiaAleatoria() que devolve uma midia já criada na biblioteca
-aleatória
+import java.util.Random;
+public class DoacaoMidia {
+    public Midia pegarMidiaAleatoria() {
+        Random gerador = new Random();
 
-Esse classe deve conter método assisitirFilme(Dvd dvd) e chamar os dois métodos de legenda
-     */
+        Midia midia1 = new Cd("1",58.41,"1989","Taylor Swift", 19);
+        Midia midia2 = new Dvd("2", 58.90, "Sing 2", "Inglês", false, "Inglês");
+        Midia midia3 = new Cd("3",39.90,"Nevermind","Nirvana", 12);
+        Midia midia4 = new Dvd("4", 22.95, "Ratatouille", "Inglês", true, "Português");
+
+        int midiaAleatória = gerador.nextInt(4);
+
+        switch (midiaAleatória) {
+            case 0:
+                return midia1;
+            case 1:
+                return midia2;
+            case 2:
+                return midia3;
+            default:
+                return midia4;
+        }
+    }
+    public void assistirFilme(Dvd dvd) {
+        // chamar os dois métodos de legenda
+    }
 }
