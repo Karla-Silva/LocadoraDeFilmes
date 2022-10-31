1e767c6228578c73bd33e68603b4573763ea9d75
MayaCarv <mayaracarvalho@hotmail.com>
2022-10-31 10:48:49 -0300

    main criada

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 package GIT.FinalProject.src;
 public class Main {
     public static void main(String[] args) {
-        /*
-        O dono de uma locadora percebeu que alguns filmes do seu estoque não estão mais sendo alugados.
-Para liberar espaço no estoque e comprar novos CD's e DVD's resolveu doar os que estão parados
-apenas ocupando espaço. Crie um sistema que faça a doação dessas mídias de forma aleatória.
-
-        Método main: O main deve pegar uma midia aleatoria e dar o play
-         */
-
-
+        Midia midia = new DoacaoMidia().pegarMidiaAleatoria();
+        System.out.println(midia.toString());
+        midia.darPlay();
     }
 }
 
